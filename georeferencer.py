ed99dced24595766a3053a613ed4ed143d9d00b5
Zach <zrb@umd.edu>
2017-08-02 12:51:53 -0400

    changed from writing BAT to calling GDAL library directly
    in order to use GDAL you must have it installed 
    
    I used the OSGeo4W installer from http://download.osgeo.org/osgeo4w/osgeo4w-setup-x86_64.exe

--- georeferencer.py
+++ georeferencer.py
@@ -5,23 +5,25 @@ Created on Jul 20, 2017
 '''
 
 import os
+from osgeo import gdal
 
 input_dir = 'B:/Workspaces/QGIS/VIRDE/NS67/tiff/'
 output_dir = 'B:/Workspaces/QGIS/VIRDE/NS67/geotiff/'
 
 input_csv = 'B:/Workspaces/R/VIRDE/georeference.csv'
-output_script_file = 'B:/Workspaces/Python/georeferencer/georeferencer.bat'
 
+for row in open(input_csv, 'r').readlines():
+    row_data = row.replace(' ', '').replace('"', '').replace('\n', '').split(',')
+    input_path = input_dir + row_data[1]
+    output_path = output_dir + row_data[1]
+    if os.path.isfile(input_path) and 'NA' not in row_data:
+        gcp_list = list()
+        
+        gcp_list.append(gdal.GCP(float(row_data[9]), float(row_data[10]), 0, 0, 0))
+        gcp_list.append(gdal.GCP(float(row_data[11]), float(row_data[12]), 0, 3296, 0))
+        gcp_list.append(gdal.GCP(float(row_data[13]), float(row_data[14]), 0, 0, 2464))
+        gcp_list.append(gdal.GCP(float(row_data[15]), float(row_data[16]), 0, 3296, 2464))
 
-with open(output_script_file, 'w') as output_script:
-    for row in open(input_csv, 'r').readlines():
-        row_data = row.replace(' ', '').replace('"', '').replace('\n', '').split(',')
-        image_path = input_dir + row_data[1]
-        if os.path.isfile(image_path) and 'NA' not in row_data:
-            output_string = os.path.join('A:\\', 'OSGeo4W64', 'bin', 'gdal_translate.exe') + ' -of GTiff -a_srs EPSG:4326 '
-            output_string += '-gcp 0 0 %s %s ' % (row_data[9], row_data[10])
-            output_string += '-gcp 3296 0 %s %s ' % (row_data[11], row_data[12])
-            output_string += '-gcp 0 2464 %s %s ' % (row_data[13], row_data[14])
-            output_string += '-gcp 3296 2464 %s %s ' % (row_data[15], row_data[16])
-            output_string += '"%s" "%s" \n' % (image_path, output_dir + row_data[1])
-            output_script.write(output_string)
+        print "Translating %s to %s" % (input_path, output_path)
+
+        gdal.Translate(srcDS = input_path, destName = output_path, GCPs = gcp_list, outputSRS = 'EPSG:4326')
